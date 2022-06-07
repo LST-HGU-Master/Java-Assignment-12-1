5ab24cc68bdde3bea420a7570445ef283f54b547
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-07 16:40:46 +0900

    Update ProgC1.java

--- src/main/java/ProgC1.java
+++ src/main/java/ProgC1.java
@@ -1,10 +1,10 @@
 public class ProgC1 {
 
-	public static void main(String[] args) {
-		DNA dna = new DNA();
-		dna.str = "ATTGAACGGTTGTGTTGATGCATCAGAATGGTTT";
-		System.out.println(dna.getPair());
-	}
+    public static void main(String[] args) {
+        DNA dna = new DNA();
+        dna.str = "ATTGAACGGTTGTGTTGATGCATCAGAATGGTTT";
+        System.out.println(dna.getPair());
+    }
 }
 
 
